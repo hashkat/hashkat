29da8fad8c0bba0a19098087c4cb72a3cab4145f
Kevin Ryczko <kevinryczko@me.com>
2013-10-13 20:36:12 -0400

    added KMC code for creating users

--- simple.cpp
+++ simple.cpp
@@ -1,55 +1,73 @@
 #include <iostream>
 #include <fstream>
 #include <time.h>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-	int array[10][100]={};
+	int array[10][10000]={};
 
 	srand(time(NULL));
 	
-	double addmember = 0.1;
-	double follow = 0.5;
+	double addjoe = 0.5;
+	double addceleb = 0.1;
+	double joefollow = 0.1;
+	double celebfollow = 0.5;
 
-	double rand_num;
-	int rand_cola, rand_rowa, rand_colb, rand_rowb;
-	int counter = 0;
-
-	ofstream output;
-	output.open("growth.dat");
-
-	while (counter < 900)
-	{
-		counter ++;
-		rand_num = (rand() % 100) / 100.0;
-		rand_cola = rand() % 100;
-		rand_rowa = rand() % 10;
-		rand_colb = rand() % 100;
-		rand_rowb = rand() % 10;
-		
-		if (rand_num < follow && rand_cola != rand_colb && rand_rowa != rand_rowb)
-		{
-			array[rand_rowa][rand_cola] = rand_rowb;
-		}
+	int njoes = 10;
+	int ncelebs = 2;
+	int nmembers = njoes + ncelebs;	
 	
-		output << "ITERATIVE STEP: " << counter << " ; rate = " << rand_num << "\n\n";	
-			
-		for (int i = 0; i < 10; i ++)
-		{
-			for (int j = 0; j < 100; j ++)
-			{
-				output << array[i][j] << " ";
-				if (j == 99)
-				{
-					output << "\n";
-				}
-			}
-		}
+	vector <int> celebs;
+	vector <int> joes;
+
+	for (int i = 0; i < 10; i ++)
+        {
+                if ( i == 2 || i == 5)
+		{celebs.push_back(i);}
+		else
+		{joes.push_back(i);}
 	}
-output.close();
+
+	int t = 0;
+	
+	while (t  < 5000)
+	{
+		double rand_prob = (rand() % 100) / 100.;
+		if ( rand_prob < addjoe)
+		{
+			njoes ++;
+			nmembers = ncelebs + njoes;
+			joes.push_back(nmembers);
+		}
+		if ( rand_prob < addceleb)
+		{
+			ncelebs ++; 
+			nmembers = ncelebs + njoes;
+			celebs.push_back(nmembers);
+		}
+		
+		cout << "At t = " << t << ".\nThere are " << nmembers << " members. " << ncelebs << " are celebs and " << njoes << " are average people." << endl;
+		
+		if (rand_prob < celebfollow)
+		{
+			
+
+		t ++;
+	}
 return 0;
 }
 	
-	
+	   
+
+
+
+
+
+
+
+
+
+
