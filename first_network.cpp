fd084ab273cf3580ba4622df15f52f3c60a3ffda
Kevin Ryczko <kevinryczko@me.com>
2013-10-17 12:14:28 -0400

    updated code after meeting, more to come

--- first_network.cpp
+++ first_network.cpp
@@ -7,33 +7,7 @@
 
 using namespace std;    // use the above libraries
 
-// this finds the first 0 value in the following column
-int first_zero(int val, int array[][3000])
-{
-	int i = 0;
-	while( i < 1000 )
-	{
-		if (array[i] == 0)
-		{
-			break;
-		}
-	}
-return i;
-}
 
-// This function is so one cant follow themselves
-int not_same_index(int value, int nodes)
-{
-	srand(time(NULL));
-
-	int rand_val = rand() % nodes;
-	while (rand_val == value)
-	{
-		rand_val = rand() % nodes;
-	}
-return rand_val;
-}	
-	
 
 int main()    // this is the main function, returns 0 always
 {
@@ -42,55 +16,51 @@ int main()    // this is the main function, returns 0 always
 
 	double t_final = 100000000.0; // 100 million minutes
 	double t = 0.0;    // the initial time is 0 minutes
-
+	double n_users = 2; // There are two nodes initially
+	
 	//define the rates
-	double add_node = 1/86400.0;
-	double bond_node = 1/604800.0; 
-	double tweet = 1/3600.0;
+	double add_node_rate = 1/86400.0; // one day
+	double bond_node_rate = 1/604800.0; // one week
+	double tweet_rate = 1/3600.0; // one hour
 
 	
-	// define the cumulative function
-	double R_1(add_node), R_2(add_node + bond_node), R_3(add_node + bond_node + tweet);
-	double cm_func[3] = {R_1, R_2, R_3};
+	// define the cumulative variables
+	double r_add = add_node_rate, r_bond = bond_node_rate*n_users, r_tweet = tweet_rate*n_users;
 
+	// define the cumulative function and cumulative bin
+	double r_total =  r_add + r_bond + r_tweet;
+	
 	vector <char> node_type; // All we have right now is joes denoted as A
 				 // A = joe, B = corporation, C = celeb --> future plans :)
 	node_type.push_back('A');
 	node_type.push_back('A');	
 
-	int n_nodes = 2; // start out with 2 members
-	
-	int network[1000][3000] = {}; // This is the main network array
+	// defining our network arrays
+	int max_users = 30000;  // max number of users -> small for now
+	int max_following = 1000; // max number of followers per user --> seemed reasonable after being discussed 
+
+	int NETWORK[max_users][max_following]; // This is the main network array
+
+	int NFOLLOWING[max_users];
+
+	//Initialize these above arrays
+	for (int i = 0; i < max_users; i ++)
+	{
+		NFOLLOWING[i] = 0;
+		for (int j = 0; j < max_following; j ++)
+		{
+			NETWORK[i][j] = -1; // Used instead of 0, -1 shows no action in the array
+		}
+	}
 
 	while ( t < t_final )
 	{
-		for (int i = 0; i < n_nodes; i ++)
-		{
-
 			// get the first uniform number
 			double u_1 = (rand() % 100) / 100.0;
 		
-			if ( R_1 > u_1*R_3 )
+			if ( u_1 < add_node_rate)
 			{
-				// if true add node
-				n_nodes ++;
-				node_type.push_back('A');
-				cout << "Time = " << t << ". Number of nodes = " << n_nodes << endl;
-			}
-
-			if ( R_2 > u_1*R_3 && R_1 < u_1*R_3 )
-			{
-				// if true create bond between with random node
-				int zero = first_zero(i, network);
-				int follow = not_same_index(i, n_nodes);						   network[zero][i] = follow;
-				cout << "Time = " << t << ". Bond formed.\n";		
-			}
-
-			if ( R_3 > u_1*R_3 && R_1 < u_1*R_3 && u_1*R_3 )
-			{
-				// if true, then user tweets but will take up alot of space
-				cout << "Time = " << t << ". Someone tweeted.\n";
-			}
+					
 
 			//get second uniform number
 			double u_2 = (rand() % 100) / 100.0;
