996fdee14c792dd5e24faf2db0936ac36ea22068
Kevin Ryczko <kevinryczko@me.com>
2013-10-16 22:20:14 -0400

    added new code

---
+++ first_network.cpp
@@ -0,0 +1,73 @@
+#include <iostream>    // input/output
+#include <fstream>     // input/output files
+#include <time.h>      // for rand functions
+#include <vector>      // for dynamic array allocations
+#include <cmath>       // for log function	
+
+using namespace std;    // use the above libraries
+
+
+
+int main()    // this is the main function, returns 0 always
+{
+	// to get random times
+	srand(time(NULL));
+
+	double t_final = 100000000.0; // 100 million minutes
+	double t = 0.0;    // the initial time is 0 minutes
+
+	//define the rates
+	double add_node = 1/86400.0;
+	double bond_note = 1/604800.0; 
+	double tweet = 1/3600.0;
+
+	
+	// define the cumulative function
+	double R_1(add_node), R_2(add_node + bond_node), R_3(add_node + bond_node + tweet);
+	double cm_func[3] = {R_1, R_2, R_3};
+
+	vector <char> node_type; // All we have right now is joes denoted as A
+				 // A = joe, B = corporation, C = celeb --> future plans :)
+	node_type.push_back('A');
+	node_type.push_back('A');	
+
+	int n_nodes = 2; // start out with 2 members
+	
+	long int network[1000][300 000]; // This is the main network array
+
+	while ( t < t_final )
+	{
+		for (int i = 0; i < n_nodes; i ++)
+		{
+
+			// get the first uniform number
+			double u_1 = (rand() % 100) / 100.0;
+		
+			if ( R_1 > u_1*R_3 )
+			{
+				// if true add node
+				n_nodes ++;
+				node_type.push_back('A');
+			}
+
+			if ( R_2 > u_1*R_3 && R_1 < u_1*R_3 )
+			{
+				// if true create bond between with random node
+				
+					
+			}
+			if ( R_3 > u_1*R_3 && R_1 < u_1*R_3 && u_1*R_3 )
+			{
+			}
+	}
+		
+
+
+
+
+
+
+
+	
+return 0;
+}
