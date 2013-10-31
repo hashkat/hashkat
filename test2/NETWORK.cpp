b16493e143ed2d001443cda2d65b5e0fed9f04ff
Kevin Ryczko <kevinryczko@me.com>
2013-10-31 14:32:20 -0400

    updated code

--- test2/NETWORK.cpp
+++ test2/NETWORK.cpp
@@ -5,7 +5,6 @@
 #include <time.h>
 #include <stdlib.h>	
 #include "INFILE.h"
-#include <iomanip>
 
 using namespace std;    // use the above libraries
 
@@ -15,11 +14,10 @@ int main()    // this is the main function, returns 0 always
 	//GET INFO FROM INFILE
     //#####################################################
 
-	long long int N_USERS = INFILE("N_USERS");
+	int N_USERS = INFILE("N_USERS");
 	int MAX_USERS = INFILE("MAX_USERS");
 	int MAX_FOLLOWING = INFILE("MAX_FOLLOWING");
 	int OUTPUT = INFILE("OUTPUT");	
-	int RANDOM_INCR = INFILE("RANDOM_INCR");
 	
 	double T_FINAL = INFILE("T_FINAL");
 	
@@ -31,15 +29,15 @@ int main()    // this is the main function, returns 0 always
 
 	//DEFINE SOME VARIABLES NEEDED
 	//#############################################################
-	long double TIME = 0.0;    // the initial time is 0 minutes
+	double TIME = 0.0;    // the initial time is 0 minutes
 	
-	long double R_TOTAL =  R_ADD + R_FOLLOW*N_USERS + R_TWEET*N_USERS; 
+	double R_TOTAL =  R_ADD + R_FOLLOW*N_USERS + R_TWEET*N_USERS; 
 	
 	//Normalize the rates
 	
-	long double R_ADD_NORM = R_ADD / R_TOTAL;
-	long double R_FOLLOW_NORM = R_FOLLOW * N_USERS  / R_TOTAL;
-	long double R_TWEET_NORM = R_TWEET * N_USERS / R_TOTAL;
+	double R_ADD_NORM = R_ADD / R_TOTAL;
+	double R_FOLLOW_NORM = R_FOLLOW * N_USERS  / R_TOTAL;
+	double R_TWEET_NORM = R_TWEET * N_USERS / R_TOTAL;
 	//#############################################################
 	
 	/*FUTURE PLAN IDEAS
@@ -63,16 +61,16 @@ int main()    // this is the main function, returns 0 always
 	
 	//DECLARE THE MAIN NETWORK ARRAY
 	//######################################################################
-	long long int NETWORK[MAX_USERS][MAX_FOLLOWING]; // This is the main network array
+	int NETWORK[MAX_USERS][MAX_FOLLOWING]; // This is the main network array
 	ofstream NETWORK_ARRAY;
 	NETWORK_ARRAY.open("NETWORK_ARRAY");
 	
-	long long int NFOLLOWING[MAX_USERS];
+	int NFOLLOWING[MAX_USERS];
 	ofstream NFOLLOWING_DATA;
 	NFOLLOWING_DATA.open("NFOLLOWING");
 	
 	// INITIALIZE THE ABOVE ARRAYS
-	for (long long int i = 0; i < MAX_USERS; i ++)
+	for (int i = 0; i < MAX_USERS; i ++)
 	{
 		NFOLLOWING[i] = 0;
 		for (int j = 0; j < MAX_FOLLOWING; j ++)
@@ -91,7 +89,7 @@ int main()    // this is the main function, returns 0 always
 	//###############################################
 
 	//CHECK OUR NUMBER OF STEPS, TWEETS and FOLLOWS
-	long long int NSTEPS = 0, N_TWEETS = 0, N_FOLLOWS = 0;
+	int NSTEPS = 0, N_TWEETS = 0, N_FOLLOWS = 0;
 
 	//RANDOM SEED
 	srand(time(NULL));
@@ -133,27 +131,18 @@ int main()    // this is the main function, returns 0 always
                         cout << "Disaster, event out of bounds" << endl;
                         }
 			// IT there should be different levels of verbosity. 0=debug, 1=normal, 2=no data writing at all
-			if (OUTPUT == 0)
-			{ DATA_TIME << fixed << setprecision(10) << "Increment: " << NSTEPS <<  "     Time: " << TIME << "     Random Number 1: " << u_1 << "     N_Users: " << N_USERS << "     N_Follows: " << N_FOLLOWS << "     N_Tweets: " << N_TWEETS << "     R_Add_Norm: " << R_ADD_NORM << "     R_Follow_Norm: " << R_FOLLOW_NORM << "     R_Tweet_Norm: " << R_TWEET_NORM << endl;
+			if (OUTPUT == 1)
+			{ DATA_TIME << "Increment: " << NSTEPS <<  "     Time: " << TIME << "     Random Number 1: " << u_1 << "     N_Users: " << N_USERS << "     N_Follows: " << N_FOLLOWS << "     N_Tweets: " << N_TWEETS << "     R_Add_Norm: " << R_ADD_NORM << "     R_Follow_Norm: " << R_FOLLOW_NORM << "     R_Tweet_Norm: " << R_TWEET_NORM << endl;
 			}
-			else if (OUTPUT == 1)
-			{
-				DATA_TIME << fixed << setprecision(5) << "TIME (min):" << "\t" << TIME << "\t" << "NUMBER OF USERS:" << "\t" << N_USERS << "\t" << "NUMBER OF FOLLOWS:" << "\t" << N_FOLLOWS << "\t" << "NUMBER OF TWEETS:" << "\t" << N_TWEETS << endl;
-			}
+			
 
-			if (RANDOM_INCR == 1)
-			{
-				// get second random number
-				double u_2 = (rand() % 1000 + 1) / 1000.0;
-				
-				// increment by random time
-				TIME += -log(u_2)/R_TOTAL;
-			}
-			else
-			{
-				TIME += 1/R_TOTAL;
-			}
-
+			//get second uniform number
+			double u_2 = (rand() % 1000 + 1) / 1000.0;
+			// increment by random time
+                        // IT make it an option to incriment the clock either through random number or 1/R. If you choose 1/R, don't bother with the random number
+			TIME += -log(u_2)/R_TOTAL;
+			//TIME += 1/R_TOTAL;
+			
 			NSTEPS ++;
 			
 			//update the rates if n_users has changed
@@ -167,15 +156,13 @@ int main()    // this is the main function, returns 0 always
 	
 if (TIME < T_FINAL)
 	{
-		cout << "\nReached maximum number of users.\n\n- - Program Terminated - -\n\n";
+		cout << "Reached maximum number of users.\n\n- - Program Terminated - -\n\n";
 	}
 	else
 	{
-		cout << "\nReached maximum amount of time.\n\n- - Program Terminated - -\n\n";
+		cout << "Reached maximum amount of time.\n\n- - Program Terminated - -\n\n";
 	}
 	DATA_TIME.close();
-	int blah = 30000000;
-	cout << blah << endl;
 			
 return 0;
 }
