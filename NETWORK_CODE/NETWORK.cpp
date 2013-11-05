864cc875419711aa98a180c452b07da5fc8f5910
Isaac Tamblyn <isaac.tamblyn@gmail.com>
2013-11-05 17:39:17 -0500

    added some comments for KR. Started to fix the issue with random number generation

--- NETWORK_CODE/NETWORK.cpp
+++ NETWORK_CODE/NETWORK.cpp
@@ -32,7 +32,7 @@ int main()    // this is the main function, returns 0 always
 	//DEFINE SOME VARIABLES NEEDED
 	//#############################################################
 	double TIME = 0.0;    // the initial time is 0 minutes
-	
+        double zerotol = 1e-16;	
 	double R_TOTAL =  R_ADD + R_FOLLOW*N_USERS + R_TWEET*N_USERS; 
 	
 	//Normalize the rates
@@ -40,26 +40,6 @@ int main()    // this is the main function, returns 0 always
 	double R_ADD_NORM = R_ADD / R_TOTAL;
 	double R_FOLLOW_NORM = R_FOLLOW *N_USERS  / R_TOTAL;
 	double R_TWEET_NORM = R_TWEET *N_USERS / R_TOTAL;
-	//#############################################################
-	
-	/*FUTURE PLAN IDEAS
-	//#####################################################################
-	vector <char> PEOPLE_TYPE; // All we have right now is joes denoted as A
-				 // A = joe, B = corporation, C = celeb --> future plans 
-	for (int i = 0; i < N_JOES; i ++)
-	{
-		PEOPLE_TYPE.push_back('A');
-	}
-	for (int i = 0; i < N_CELEB; i ++)
-	{
-		PEOPLE_TYPE.push_back('C');
-	}
-	for (int i = 0; i < N_ORG; i ++)
-	{
-		PEOPLE_TYPE.push_back('B');
-		
-	}*/
-	//#####################################################################
 	
 	//DECLARE THE MAIN NETWORK ARRAY
 	//######################################################################
@@ -68,6 +48,8 @@ int main()    // this is the main function, returns 0 always
 	int NFOLLOWING[MAX_USERS];
 	
 	// INITIALIZE THE ABOVE ARRAYS
+        // IT do this in a separate file
+
 	for (int i = 0; i < MAX_USERS; i ++)
 	{
 		NFOLLOWING[i] = 0;
@@ -87,6 +69,7 @@ int main()    // this is the main function, returns 0 always
 	//###############################################
 
 	//CHECK OUR NUMBER OF STEPS, TWEETS and FOLLOWS
+
 	long int N_STEPS = 0, N_TWEETS = 0, N_FOLLOWS = 0;
 
 	//RANDOM SEED
@@ -97,31 +80,41 @@ int main()    // this is the main function, returns 0 always
 	while ( TIME < T_FINAL && N_USERS < MAX_USERS )
 	{
 		
-			// get the first uniform number
-                        // IT confirm that you have enough precision here - confirmed
-			 double u_1 = ((long) rand() % 10000000 + 1) / 10000000.0;	
-
+			// get the first uniform number [0,1). This selects a number from a long int (default RAND_MAX)
+                        // IT confirm this is still uniform
+ 
+			 double u_1 = (rand()+1) / (double(RAND_MAX)+1);
+                        // IT it seems like u_1 has more than 16 digits...what's with that?
+                         
 			// DECIDE WHAT TO DO
 			//##############################################################################
 			// If we find ourselves in the add user chuck of our cumuative function
-			if (u_1 - R_ADD_NORM <= 1e-15 )
+
+                        // IT are we happy with this value of zerotol???
+			if (u_1 - R_ADD_NORM <= zerotol )
 			{
 				N_USERS ++;
 				//call to function to decide which user to add
 			}
 
 			// If we find ourselves in the bond node chunk of our cumulative function
-			else if (u_1 - (R_ADD_NORM + R_FOLLOW_NORM) <= 1e-15 )
+
+                        // IT do we have enough precision to deal with the case of 30 million users???
+			else if (u_1 - (R_ADD_NORM + R_FOLLOW_NORM) <= zerotol )
 			{
 				N_FOLLOWS ++;
 				double val = u_1 - R_ADD_NORM;
 				int user = val / (R_FOLLOW_NORM / N_USERS);  // this finds the user
-				NETWORK[user][NFOLLOWING[user]] = rand() % N_USERS;
+
+                                // IT do a call to the follow function, i.e.
+                                // IT confirm the user selection for random is correct (check to make sure boundaries work!)
+ 
+				NETWORK[user][NFOLLOWING[user]] = int((rand()/double(RAND_MAX))*N_USERS);
 				NFOLLOWING[user] ++;
 			}
 			
 			// if we find ourselves in the tweet chuck of the cumulative function
-			else if (u_1 - (R_ADD_NORM + R_FOLLOW_NORM + R_TWEET_NORM) <= 1e-15)
+			else if (u_1 - (R_ADD_NORM + R_FOLLOW_NORM + R_TWEET_NORM) <= zerotol )
 			{
 				N_TWEETS ++;
 				double val = u_1 - R_ADD_NORM - R_FOLLOW_NORM;
@@ -137,8 +130,6 @@ int main()    // this is the main function, returns 0 always
 			// we need that 2, but when n_users is fixed we dont need the 2			
 			double DYNAMIC_ADD_RATE = N_USERS / TIME, DYNAMIC_FOLLOW_RATE = 2*N_FOLLOWS / (N_USERS*TIME), DYNAMIC_TWEET_RATE = 2*N_TWEETS / (N_USERS*TIME);
 			
-			// IT there should be different levels of verbosity. 0=debug, 1=normal, 2=no data writing at all
-			// LEVELS OF VERBOSITY
 			// ##################################################################################################
 			if (VERBOSE == 0)
 			{ DATA_TIME << fixed << setprecision(10) << "INCREMENT: " << N_STEPS << "\t" << "TIME_(min): " << TIME << "\t" << "RANDOM_NUM1: " << u_1 << "\t" << "N_USERS: " << N_USERS << "\t" << "N_FOLLOWS: " << N_FOLLOWS << "\t" << "N_TWEETS: " << N_TWEETS << "\t" << "R_ADD_NORM: " << R_ADD_NORM << "\t" << "R_FOLLOW_NORM: " << R_FOLLOW_NORM << "\t" << "R_TWEET_NORM: " << R_TWEET_NORM << "\t" << "DYNAMIC_ADD_RATE: " << DYNAMIC_ADD_RATE << "\t" << "DYNAMIC_FOLLOW_RATE: " << DYNAMIC_FOLLOW_RATE << "\t" << "DYNAMIC_TWEET_RATE: " << "\t" << DYNAMIC_TWEET_RATE << endl;
@@ -156,11 +147,12 @@ int main()    // this is the main function, returns 0 always
 			if (RANDOM_INCR == 1)
 			{
 				// get second random number
-				double u_2 = (rand() % 1000 + 1) / 1000.0;
+			        double u_2 = (rand()+1) / (double(RAND_MAX)+1);
 				
 				// increment by random time
 				TIME += -log(u_2)/R_TOTAL;
 			}
+                        // SP this if else on every timestep is silly
 			else
 			{
 				TIME += 1/R_TOTAL;
@@ -171,12 +163,10 @@ int main()    // this is the main function, returns 0 always
 			//update the rates if n_users has changed
 			R_TOTAL = R_ADD + N_USERS*R_FOLLOW + N_USERS*R_TWEET ;
 			R_ADD_NORM = R_ADD / R_TOTAL; 
-			R_FOLLOW_NORM = R_FOLLOW*N_USERS  / R_TOTAL;
-			R_TWEET_NORM = R_TWEET*N_USERS  / R_TOTAL;
+			R_FOLLOW_NORM = R_FOLLOW*N_USERS / R_TOTAL;
+			R_TWEET_NORM = R_TWEET*N_USERS / R_TOTAL;
 			
 	}
-        // IT It looks like you may have issues with long vs short ints. NSTEPS went negative for one of my test cases. -> FIXED - NSTEPS is now of type 'long int' 
-	
 
 	//PRINT WHY PROGRAM STOPPED - SP needs to be more efficient as well, these if loops will slow down code
 	//#######################################################################################
