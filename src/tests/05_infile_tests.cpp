cb0c8d05a4e77a10d12aa74650a50f386848923b
Adam <domuradical@gmail.com>
2014-02-06 14:47:39 -0500

    Broken commit -- on own branch

--- src/tests/05_infile_tests.cpp
+++ src/tests/05_infile_tests.cpp
@@ -1,3 +1,5 @@
+#include <fstream>
+
 #include "tests.h"
 
 #include "../config_static.h"
@@ -8,8 +10,32 @@
 
 using namespace std;
 
+static bool str_replace(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = str.find(from);
+    if(start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
+}
+
 // Test the system in edge cases using custom INFILE's.
 // UnitTest++ will catch any segfaults, and report them as test errors
+
+ParsedConfig create_config(const char* followback_probability) {
+    const char* OUT_FILE = "src/tests/followback_temp.yaml";
+    // File with templated arguments
+    fstream input(fstream::in, "src/tests/followback_template.yaml");
+    fstream output(fstream::out, OUT_FILE);
+
+    std::string line;
+    while (getline(input, line)) {
+        str_replace(line, "$followback_probability", followback_probability);
+        output << line << '\n';
+    }
+    output.close();
+
+    return parse_yaml_configuration(OUT_FILE);
+}
 
 SUITE(SanityChecks) {
     const int TEST_SEED = 0xDEADBEEF;
