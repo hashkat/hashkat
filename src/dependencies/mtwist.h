158de612fea67b9d5fcaa3ff2f8dbcea66b110b6
adomurad <adomurad@localhost.localdomain>
2015-06-22 17:46:26 -0400

    Remove mersenne-simd

--- src/dependencies/mtwist.h
+++ src/dependencies/mtwist.h
@@ -46,15 +46,13 @@
 
 #include <vector>
 
-#include "mersenne-simd/SFMT.h"
-
 #include "dependencies/lcommon/perf_timer.h"
 
 // AD: Intrusive, but had to be done for quick implementation of serialization:
 #include "../DataReadWrite.h"
 
 // Mersenne twister random number generator
-class MTwistClassic {
+class MTwist {
 public:
     enum {
         N = 624
@@ -63,15 +61,15 @@ public:
     void init_genrand(unsigned int s);
     void init_by_array(unsigned int init_key[], int key_length);
 
-    MTwistClassic() :
+    MTwist() :
             mti(N + 1) {
     }
 
-    MTwistClassic(unsigned int s) :
+    MTwist(unsigned int s) :
             mti(N + 1) {
         init_genrand(s);
     }
-    MTwistClassic(unsigned int init_key[], int key_length) :
+    MTwist(unsigned int init_key[], int key_length) :
             mti(N + 1) {
         init_by_array(init_key, key_length);
     }
@@ -164,119 +162,6 @@ public:
 private:
     unsigned int mt[N];
     int mti;
-};
-
-typedef MTwistClassic MTwist;
-
-// TODO: Remove the duplication once MTWistSSE can be trusted
-// Mersenne twister random number generator, SSE optimized
-class MTwistSSE {
-public:
-    void init_genrand(unsigned int s) {
-        sfmt_init_gen_rand(&state, s);
-
-    }
-    void init_by_array(unsigned int init_key[], int key_length) {
-        sfmt_init_by_array(&state, init_key, key_length);
-    }
-
-    MTwistSSE(unsigned int s = 0) {
-        init_genrand(s);
-    }
-    MTwistSSE(unsigned int init_key[], int key_length) {
-        init_by_array(init_key, key_length);
-    }
-
-    /* generates a random number on [0,0xffffffff]-interval */
-    unsigned int genrand_uint32(void) {
-        PERF_TIMER();
-        return sfmt_genrand_uint32(&state);
-    }
-
-    /* generates a random number on [0,1]-real-interval */
-    double genrand_real1(void) {
-        PERF_TIMER();
-        return sfmt_genrand_real1(&state);
-    }
-
-    /* generates a random number on [0,1)-real-interval */
-    double genrand_real2(void) {
-        PERF_TIMER();
-        return sfmt_genrand_real2(&state);
-    }
-
-    /* generates a random number on (0,1)-real-interval */
-    double genrand_real3(void) {
-        PERF_TIMER();
-        return sfmt_genrand_real3(&state);
-    }
-
-    /* generates a random number on [0,1) with 53-bit resolution*/
-    double genrand_res53(void) {
-        PERF_TIMER();
-        return sfmt_genrand_res53(&state);
-    }
-
-// AD: Added for Twitter simulation
-
-    /* Grab an integer from 0 to max, non-inclusive (ie appropriate for array lengths). */
-    int rand_int(int max) {
-        PERF_TIMER();
-        //AD: Modified to remove modulo-bias problem. Inspired by Java's nextInt implementation.
-        long long raw = genrand_uint32();
-
-        if ((max & -max) == max) { // i.e., max is a power of 2
-            return (int) ((max * raw) >> 31);
-        }
-        long long val = raw % max;
-        // Reject values within a small, problematic range:
-        while (raw - val + (max - 1) < 0) {
-            raw = genrand_uint32();
-            val = raw % max;
-        }
-        return val;
-    }
-    int rand_int(int min, int max) {
-        int range = max - min;
-        return rand_int(range) + min;
-    }
-
-    // Makes a choice given a profile of probabilities
-    inline int kmc_select(double* start, int len) {
-        double num =  genrand_real1();
-        for (int i = 0; i < len; i++) {
-            if (num < start[i]) {
-                return i;
-            }
-            num -= start[i];
-        }
-        return len - 1; // Assume floating point error
-    }
-
-    /* Grab a real number within [0,1) with 53-bit resolution */
-    double rand_real_not1() {
-        return genrand_res53();
-    }
-    /* Grab a real number within (0,1] with 53-bit resolution */
-    double rand_real_not0() {
-        return 1.0 - rand_real_not1();
-    }
-
-    template <typename T>
-    T pick_random_uniform(const std::vector<T>& vec) {
-        int n = rand_int(vec.size());
-        return vec[n];
-    }
-    bool random_chance(double probability) {
-        return (genrand_real1() < probability);
-    }
-    /* Using Mersenne-twister, grab a real number within [0,1] */
-    double rand_real_with01() {
-        return genrand_real1();
-    }
-
-private:
-    sfmt_t state;
 };
 
 #endif
