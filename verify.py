ef3ce8f5bf68f77ca86199f1348b59c03869324a
Isaac Tamblyn <isaac.tamblyn@gmail.com>
2015-07-03 16:12:03 -0400

    cleaned up verify.py

--- verify.py
+++ verify.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 
+'''
+This script checks to see whether the current build of hashkat matches the reference data in referencefiles/testXX/output
+'''
+
 import filecmp
 import os.path
 
-print "The files printed below are those in which have an output different from what was expected. If no files are printed, there was no discrepancies"
-
 listoffiles = ['Bot_info', 'Categories_Distro', 'Celebrity_info', 'cumulative-degree_distribution_month_000', 'dd_by_follow_model', 'in-degree_distribution_month_000', 'main_stats', 'model_match', 'network', 'Organization_info', 'out-degree_distribution_month_000', 'region_connection_matrix_month_000', 'retweets_distro', 'Standard_info', 'tweets_distro'] 
+
+no_differences_detected = True
 
 for file in listoffiles:
 
     if os.path.exists(file + '.dat') == True:
 
         if filecmp.cmp( file + '.dat', file + '.ref', shallow = False) == False:
-
+            no_differences_detect = False
+            print 'Test failed, difference between test and reference data'
             print file
+
+if (no_differences_detected == False):
+    print 'All tests passed'
