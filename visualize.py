8421bbd4e0af1f3ed2d64ccb9fd6c108781dfce3
nbuhagiar <nicholas.buhagiar@uoit.net>
2015-09-08 19:29:36 -0400

    Adding more functions to visualize.py

--- visualize.py
+++ visualize.py
@@ -4,9 +4,16 @@
 
 import matplotlib.pyplot as plt
 import networkx as nx
+from operator import itemgetter
 
 G = nx.read_edgelist('output/network.dat')
+pos = nx.spring_layout(G,iterations=75)
 
-nx.draw(G)
+nx.draw(G,pos)
 plt.savefig('graph.svg')
-plt.show()
+#plt.show()
+print('Graph Density: ' + str(nx.density(G)))
+print('Number of Nodes: ' + str(nx.number_of_nodes(G)))
+print('Number of Edges: ' + str(nx.number_of_edges(G)))
+print('Most Popular Agent ID and Number of Connections ' + str(sorted(G.degree_iter(),key=itemgetter(1),reverse=True)[0:1]))
+
