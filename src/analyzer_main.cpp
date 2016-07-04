3ab3a1e12f215ec1ed62d3b8c8dd5542287cefef
jig08 <grover.jigyasa1@gmail.com>
2016-07-04 12:58:11 -0400

    Formatting JSON Object.

--- src/analyzer_main.cpp
+++ src/analyzer_main.cpp
@@ -665,10 +665,10 @@ struct Analyzer {
         tweet_stream_file << "{" << "\n";
         for (auto& t : atl) {
             tweet_stream_file << "{" <<
-                              "\"ID:\"" << t.id_tweeter << ",\n"
-                              << "\"origID:\"" << t.content->id_original_author << ",\n"
-                              << "\"time:\"" << t.creation_time << ",\n"
-                              << "\"origTime\"" << t.content->time_of_tweet << "\n"
+                              "\"ID\":" << t.id_tweeter << ",\n"
+                              << "\"origID\":" << t.content->id_original_author << ",\n"
+                              << "\"time\":" << t.creation_time << ",\n"
+                              << "\"origTime\":" << t.content->time_of_tweet << "\n"
                               << "}," << "\n";
         }
 
