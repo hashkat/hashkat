54c82020ecded4d2dc9a43555643048f212b8342
jig08 <grover.jigyasa1@gmail.com>
2016-07-06 16:12:12 -0400

    Timer for transition between options in visualizations.

--- hashkat_visualizations/js/main.js
+++ hashkat_visualizations/js/main.js
@@ -1,58 +1,59 @@
 if (!Detector.webgl) {
-        Detector.addGetWebGLMessage();
-    } else {
+    Detector.addGetWebGLMessage();
+} else {
 
-        var years = ['1', '2', '3'];
-        var container = document.getElementById('container');
-        var globe = new DAT.Globe(container);
-        console.log(globe);
-        var i, tweens = [];
+    var options = ['1', '2', '3'];
+    var container = document.getElementById('container');
+    var globe = new DAT.Globe(container);
+    console.log(globe);
+    var i, tweens = [];
 
-        var settime = function(globe, t) {
-            return function() {
-                new TWEEN.Tween(globe).to({
-                    time: t / years.length
-                }, 500).easing(TWEEN.Easing.Cubic.EaseOut).start();
-                var y = document.getElementById('option' + years[t]);
-                if (y.getAttribute('class') === 'option active') {
-                    return;
-                }
-                var yy = document.getElementsByClassName('option');
-                for (i = 0; i < yy.length; i++) {
-                    yy[i].setAttribute('class', 'option');
-                }
-                y.setAttribute('class', 'option active');
-            };
-        };
-
-        for (var i = 0; i < years.length; i++) {
-            var y = document.getElementById('option' + years[i]);
-            y.addEventListener('mouseover', settime(globe, i), false);
-        }
-
-        var xhr;
-        TWEEN.start();
-
-
-        xhr = new XMLHttpRequest();
-        xhr.open('GET', './json/newtest.json', true);
-        xhr.onreadystatechange = function(e) {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    var data = JSON.parse(xhr.responseText);
-                    window.data = data;
-                    for (i = 0; i < data.length; i++) {
-                        globe.addData(data[i][1], {
-                            format: 'magnitude',
-                            name: data[i][0],
-                            animated: true
-                        });
-                    }
-                    globe.createPoints();
-                    settime(globe, 0)();
-                    globe.animate();
-                }
+    var settime = function(globe, t) {
+        return function() {
+            new TWEEN.Tween(globe).to({
+                time: t / options.length
+            }, 500).easing(TWEEN.Easing.Cubic.EaseOut).start();
+            var y = document.getElementById('option' + options[t]);
+            if (y.getAttribute('class') === 'option active') {
+                return;
             }
+            var yy = document.getElementsByClassName('option');
+            for (i = 0; i < yy.length; i++) {
+                yy[i].setAttribute('class', 'option');
+            }
+            y.setAttribute('class', 'option active');
         };
-        xhr.send(null);
+    };
+
+    var time = 0;
+    for (var i = 0; i < options.length; i++) {
+        time = time + 5000;
+        window.setTimeout(settime(globe, i), time);
     }
+
+    var xhr;
+    TWEEN.start();
+
+
+    xhr = new XMLHttpRequest();
+    xhr.open('GET', './json/newtest.json', true);
+    xhr.onreadystatechange = function(e) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText);
+                window.data = data;
+                for (i = 0; i < data.length; i++) {
+                    globe.addData(data[i][1], {
+                        format: 'magnitude',
+                        name: data[i][0],
+                        animated: true
+                    });
+                }
+                globe.createPoints();
+                settime(globe, 0)();
+                globe.animate();
+            }
+        }
+    };
+    xhr.send(null);
+}
